06f1309478cd112569c285adeb6e3ff83765ef18
MacOG <49504334+MacOG@users.noreply.github.com>
2021-03-10 13:45:55 -0700

    Added division function

--- calculator.py
+++ calculator.py
@@ -13,3 +13,7 @@ def subtraction(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def divide(first_term, second_term):
+    return first_term / second_term
